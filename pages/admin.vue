71badf5e47a39a7918e4f831c35aa71fc4a9adf4
jasenmichael <jasen@ready-builder.com>
2020-12-20 14:29:42 -0600

    feat: implement netlify identity

--- pages/admin.vue
+++ pages/admin.vue
@@ -13,21 +13,13 @@ export default {
   mounted() {
     if (process.client) {
       // eslint-disable-line
-      NetlifyCmsApp.init({
-        // merge config with static/admin/config.yml
-        config: {
-          site_url: this.url,
-        },
-      })
-
-      setTimeout(() => {
-        // gotta wait for react to be done taking over
-        const selector = document.querySelectorAll("a[href='" + this.url + "']")
-        selector[2].target = '_self'
-        // selector.forEach((el) => {
-        //   el.target = '_self'
-        // })
-      }, 4000)
+      NetlifyCmsApp.init()
+      // NetlifyCmsApp.init({
+      //   // merge config with static/admin/config.yml
+      //   config: {
+      //     site_url: this.url,
+      //   },
+      // })
     }
   },
   head: {
